78fc99dbc3960e8270a8caff309af89a17c4a8e2
Aruldoss <marceljoseph.aruldoss@klm.com>
2021-09-15 23:30:50 +0200

    UI5_Tooling_added_V1

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use("/", express.static("webapp"));
-
-console.log("Server http://localhost:4000");
-const port = process.env.PORT || 4000;
-
-app.listen(port);
